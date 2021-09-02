82aba458fe470818356e307ae2f10d9c9b416010
Kirera Wainaina <richardwainainak@gmail.com>
2021-09-02 13:41:07 +0300

    Create a simple server
    At the moment, it is able to receive requests and print webpage.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const http2 = require("http2");
+const fs = require("fs");
+
+const port = 443;
+const options = {
+    key: fs.readFileSync("localhost-privkey.pem"),
+    cert: fs.readFileSync("localhost-cert.pem")
+};
+
+const server = http2.createSecureServer(options);
+
+server.on("request", (request, response) => {
+    console.log("Request received");
+    response.writeHead(200, { "content-type": "text/plain" })
+	.end("Hello word")
+})
+
+server.listen(port, () => console.log(`Listening on port ${port}`))
