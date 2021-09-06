0238843b8bb1c000870e21f88e2ab6370ea7a26f
Kirera Wainaina <richardwainainak@gmail.com>
2021-09-06 12:44:27 +0300

    Make a request to the autocomplete service

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -7,3 +7,11 @@ window.addEventListener("DOMContentLoaded", () => {
 	    logoQuestion.style.animation = "1s hanging forwards";
 	})
 })
+
+const service = new google.maps.places.AutocompleteService();
+service.getPlacePredictions(
+    { input: "kahawa" },
+    (prediction, status) => {
+	console.log(prediction);
+	console.log(status)
+    });
