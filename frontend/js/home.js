9c3d96eac0373de5e488628464faf6ac09cdf107
Kirera Wainaina <richardwainainak@gmail.com>
2021-09-06 15:16:19 +0300

    Remove the previous predictions
    Also change the event to watch out for from "change" to "input".
    Input event happens frequently.

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -10,7 +10,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
 const service = new google.maps.places.AutocompleteService();
 const destination = document.getElementById("destination");
-destination.addEventListener("change", event => {
+destination.addEventListener("input", event => {
     console.log(event.target.value)
     const neKenya = new google.maps.LatLng({ lat: 4.995, lng: 42.764 });
     const swKenya = new google.maps.LatLng({ lat: -5.196, lng: 34.021 });
@@ -23,9 +23,11 @@ destination.addEventListener("change", event => {
 		displayPredictions(predictions);
 	    }
 	});
+    removePreviousPredictions();
 })
 
 function displayPredictions(predictions) {
+    console.log(predictions)
     const fragment = new DocumentFragment();
     predictions.forEach(prediction => {
 	const pEl = document.createElement("p");
@@ -34,4 +36,10 @@ function displayPredictions(predictions) {
 	fragment.appendChild(pEl);
     });
     destination.parentNode.appendChild(fragment);
+}
+
+function removePreviousPredictions() {
+    const predictions = document.querySelectorAll(".prediction");
+    predictions.forEach(
+	prediction => prediction.parentNode.removeChild(prediction))
 }
